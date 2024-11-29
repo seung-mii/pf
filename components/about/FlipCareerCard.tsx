c05aae5c2083fb41602445c376db6d49ca4b2898
withsme <tmdal9953@gmail.com>
2024-11-29 15:52:35 +0900

    fix : About Page, Career Card 뒷면 안보이는 에러

--- components/about/FlipCareerCard.tsx
+++ components/about/FlipCareerCard.tsx
@@ -10,12 +10,11 @@ export function FlipCareerCard({ category, title, details, date }: CareerInfo) {
   ));
 
   return (
-    <div
-      className="card relative min-w-[260px] h-full min-h-[330px] text-center transition-transform duration-700 ease-out cursor-pointer [transform-style:preserve-3d] hover:rotate-y-180"
+    <div className="card relative min-w-[260px] h-full min-h-[330px] flex-col justify-between p-4 text-center items-center transition-transform duration-700 [transform-style:preserve-3d] cursor-pointer"
       style={{ perspective: "1000px" }}
     >
-      <div className="front absolute w-full h-full flex flex-col justify-center rounded-[2vw] shadow-md text-base px-6 pt-2 pb-6 text-darkBlue bg-lightBlue backface-hidden">
-        <div className="flex items-center justify-center text-m font-bold text-darkBlue h-[30%] whitespace-pre-line">
+      <div className="front absolute w-full h-full font-medium overflow-hidden [backface-visibility:hidden] flex flex-col justify-center rounded-[2vw] shadow-md text-base px-6 pt-2 pb-6 text-darkBlue bg-lightBlue">
+        <div className="flex items-center justify-center text-m font-bold text-darkBlue m-6 whitespace-pre-line">
           {formattedTitle}
         </div>
         <div className="text-base text-s text-left leading-[1.4] flex-grow text-darkBlue">
@@ -23,9 +22,9 @@ export function FlipCareerCard({ category, title, details, date }: CareerInfo) {
             <p key={i} className="mb-3">{line}</p>
           ))}
         </div>
-        <div className="text-s text-darkBlue italic">{date}</div>
+        <div className="text-s text-darkBlue text-[#999] italic">{date}</div>
       </div>
-      <div className="back absolute w-full h-full flex flex-col justify-center rounded-[2vw] shadow-md text-xl font-semibold text-bottom bg-darkBlue rotate-y-180 backface-hidden">
+      <div className="back absolute w-full h-full font-medium overflow-hidden [backface-visibility:hidden] flex flex-col justify-center rounded-[2vw] shadow-md text-xl font-semibold text-bottom bg-darkBlue [transform:rotateY(180deg)]">
         {category}
       </div>
     </div>
