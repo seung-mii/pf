b838b93bff310353395533221d8c74db9e708238
withsme <tmdal9953@gmail.com>
2024-11-30 12:19:50 +0900

    style : 주요 프로젝트만 보여주는 버튼 추가

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { projectsData } from "../../data/projects";
 import TimelineItem from "@/components/projects/TimelineItem";
 import { useInfiniteScroll } from "@/hooks/useInfiniteScroll";
@@ -11,21 +11,38 @@ export default function Project() {
   const timelineRef = useRef<HTMLDivElement | null>(null);
   const [items, setItems] = useState<ProjectInfo[]>(projectsData.slice(0, 2));
   const [hasMore, setHasMore] = useState(true);
+  const [isImportantOnly, setIsImportantOnly] = useState(false);
 
-  const fetchMoreData = () => {
-    if (items.length >= projectsData.length) {
+  const fetchMoreData = useCallback(() => {
+    const filteredProjects = isImportantOnly
+      ? projectsData.filter((project) => project.isImportant)
+      : projectsData;
+
+    if (items.length >= filteredProjects.length) {
       setHasMore(false);
       return;
     }
-    const nextItems = projectsData.slice(items.length, items.length + 2);
+    const nextItems = filteredProjects.slice(items.length, items.length + 2);
     setItems((prevItems) => [...prevItems, ...nextItems]);
-  };
+  }, [items.length, isImportantOnly]);
 
   useInfiniteScroll({ hasMore, fetchMoreData, dependencies: [items] });
   useTimelineScroll(timelineRef);
 
+  useEffect(() => {
+    const filteredProjects = isImportantOnly ? projectsData.filter((project) => project.isImportant) : projectsData;
+    setItems(filteredProjects.slice(0, 2));
+    setHasMore(filteredProjects.length > 2);
+  }, [isImportantOnly]);
+
   return (
     <div ref={timelineRef} className="relative max-w-5xl mx-auto p-4 sm:p-8 font-sans">
+      <button
+        onClick={() => setIsImportantOnly((prev) => !prev)}
+        className="absolute right-5 top-16 px-4 py-3 bg-point text-darkBlue text-xs rounded-[20px]"
+      >
+        {isImportantOnly ? "Show All" : "Show Important"}
+      </button>
       <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-[2px] bg-darkBlue h-0 transition-all duration-300 ease-out timeline-line"></div>
       <div className="flex flex-col items-center mt-7">
         {items.map((item, index) => (
